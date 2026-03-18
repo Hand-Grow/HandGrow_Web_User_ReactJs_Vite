e382a20b3f913be95febfe59cde65edde93f9494
Nguyen <hoangnguyendepgiai@gmail.com>
2026-03-18 16:00:03 +0700

    ci: enable trailing slash for S3 hosting

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
   eslint: { ignoreDuringBuilds: true },
   typescript: {
     ignoreBuildErrors: true,
